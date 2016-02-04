ac09af9e718c6c74142b95c6214e8c39a4e47f9a
“test” <test@163.com>
2016-02-04 11:26:42 +0800

    android_storage_internalStorage

---
+++ android_storage_internalStorage/src/com/example/android_storage_internalstorage/MyTester.java
@@ -0,0 +1,30 @@
+package com.example.android_storage_internalstorage;
+
+import com.example.android_storage_internalstorage.file.FileService;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+import android.util.Log;
+
+public class MyTester extends AndroidTestCase {
+
+	private final String TAG="MyTester";
+	public MyTester() {
+	}
+	
+	public void Save(){
+		FileService service=new FileService(getContext());
+		/**
+		 * MODE_PRIVATE will create the file (or replace a file of the same name) and make it private to your application. 
+		 * Other modes available are: MODE_APPEND, MODE_WORLD_READABLE, and MODE_WORLD_WRITEABLE.  
+		 */
+		boolean flag=service.saveContentToFile("aa.txt", Context.MODE_PRIVATE,"ÄãºÃÂð?".getBytes());
+		Log.i(TAG, "--->>"+flag);
+	}
+	
+	public void Read(){
+		FileService service=new FileService(getContext());
+		String msg=service.readContentFromFile("login.txt");
+		Log.i(TAG, msg);
+	}
+}
